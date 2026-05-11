6d0dd1870fdd2d95ab7940a3494a80df305404a8
임지나 <jn4624@gmail.com>
2026-05-11 11:47:15 +0900

    feat: 프로젝트 초기화

---
+++ effective-software-testing/src/test/java/com/example/effectivesoftwaretesting/EffectiveSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivesoftwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveSoftwareTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
