e88c6ea172cf6c7db9c8bb991d1e1466f5b073d7
임지나 <jn4624@gmail.com>
2026-05-12 21:36:53 +0900

    feat: 첫번째 테스트

---
+++ inflearn-spring-test/src/test/java/com/jyujyu/dayonetest/LombokTestDataTest.java
@@ -0,0 +1,20 @@
+package com.jyujyu.dayonetest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LombokTestDataTest {
+
+    /**
+     * Test 어노테이션의 import 패키지가 AssertJ는
+     * junit4의 패키지이므로 jupiter import 패키지를 사용하도록 한다.
+     */
+
+    @Test
+    public void testDataTest() {
+        TestData testData = new TestData();
+        testData.setName("jyujyu");
+
+        Assertions.assertEquals("jyujyu", testData.getName());
+    }
+}
