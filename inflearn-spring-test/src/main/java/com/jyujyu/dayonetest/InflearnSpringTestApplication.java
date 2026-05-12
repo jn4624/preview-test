e149f606fad6082cbc42c0a0b77c1ad01773b2f8
임지나 <jn4624@gmail.com>
2026-05-12 21:36:26 +0900

    feat: 프로젝트 초기화

---
+++ inflearn-spring-test/src/main/java/com/jyujyu/dayonetest/InflearnSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.jyujyu.dayonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringTestApplication.class, args);
+    }
+
+}
